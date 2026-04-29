79601b85259ab23c68fc5f471ed9ba0fda566616
KevinDemo <chenning.0515@gmail.com>
2026-04-29 15:08:26 +1200

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        //1. define a dictionary to group the strs using the same char count
+        var mapper = new Dictionary<string,List<string>>();
+        //2. Loop each str and add the char count string into the mapper
+        foreach(var str in strs){
+            var charCount = new int[26]; 
+            for(int i = 0; i < str.Length; i++)
+            {
+                var charPosition = (char)str[i] - (char)'a';
+                charCount[charPosition]++;
+            }
+           
+           var charString = string.Join(",", charCount);
+            if(mapper.ContainsKey(charString))
+            {
+                mapper[charString].Add(str);
+            }
+            else
+            {
+                mapper[charString] = new List<string>{str};
+            }
+        }
+        //3. return mapper by group
+            return mapper.Values.ToList();
+    }
+}
